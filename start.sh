e75ed724d3ccb992a95cedca7e4be5d52cf1a9a2
Horizon <111064289+Horizon12275@users.noreply.github.com>
2024-07-16 22:33:25 +0800

    fix to lowercase

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-docker pull horizon12275/ActionTest:latest
-docker rm -f TG-test||true&&docker run  --name=TG-test -d -p 8082:8082 horizon12275/ActionTest:latest
+docker pull horizon12275/action-test:latest
+docker rm -f TG-test||true&&docker run  --name=TG-test -d -p 8082:8082 horizon12275/action-test:latest
 docker image prune -af
